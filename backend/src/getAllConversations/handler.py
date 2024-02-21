7bc21dd9e3552cdb259ea2c1d6166b200f5cab5d
Ethan Stein <ethanmstn33@gmail.com>
2024-02-21 08:54:15 -0500

    Add ConversationBucket, ConversationBucketBucketPolicy, ConversationTable, getConversation, getConversationLogGroup, ConversationApi, getAllConversations, addDummyConversationData

---
+++ backend/src/getAllConversations/handler.py
@@ -0,0 +1,39 @@
+import json
+import os
+
+import boto3
+
+
+def handler(event, context):
+    try:
+        limit = event["pathParameters"]["limit"]
+
+        dynamodb = boto3.resource("dynamodb")
+
+        table = dynamodb.Table(os.environ["CONVERSATIONTABLE_TABLE_NAME"])
+
+        # Get meta data from table
+        response = table.scan(
+            Limit=limit,
+            AttributesToGet=[
+                "phone",
+                "date_registered",
+                "last_contacted",
+                "city",
+                "state",
+                "id",
+            ],
+        )
+    except Exception as e:
+        return {"statusCode": 500, "body": str(e)}
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps(
+            {
+                "message": f"{response['Count']} item(s) fetched successfully",
+                "data": response["Items"],
+                "lastEvaluatedKey": response.get("LastEvaluatedKey"),
+            }
+        ),
+    }
