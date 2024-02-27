6535d4bbadb24450b09b728488f1913711716962
Ethan Stein <ethanmstn33@gmail.com>
2024-02-27 14:15:59 -0500

    Add react-router-dom package and implement routing in main.jsx

---
+++ frontend/src/Pages/Login.jsx
@@ -0,0 +1,23 @@
+import microsoft from "../assets/microsoft.svg";
+
+export default function () {
+  const handleLoginMsft = () => {
+    window.location.href =
+      "https://celly-microsoft-pool.auth.us-east-1.amazoncognito.com/oauth2/authorize?identity_provider=cellymicrosoftpool&redirect_uri=http://localhost:5173&response_type=TOKEN&client_id=5dfi8s06l6ephu5e5c6vri4aqe&scope=email openid phone";
+  };
+
+  return (
+    <div className="flex items-center justify-center mt-[35vh]">
+      <div>
+        <div className="text-lg font-bold mb-5">Welcome To Celly</div>
+        <div
+          onClick={handleLoginMsft}
+          className="flex space-x-3 rounded-lg shadow w-fit px-5 py-3 cursor-pointer hover:opacity-50"
+        >
+          <img src={microsoft} alt="microsoft-logo" className="w-5" />
+          <div className="font-[450]">Login with Microsoft</div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,5 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+import Login from "./Pages/Login.jsx";
+
+const router = createBrowserRouter([
+  { path: "/", element: <App /> },
+  { path: "/login", element: <Login /> },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
