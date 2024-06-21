5b9bda0cd7eb48d86e410178ab09bd8417cea8bc
Ethan <gamesniper99@live.com>
2024-06-21 14:31:49 -0400

    Changed style of coming soon page

--- frontend/src/Components/Views/Analytics.jsx
+++ frontend/src/Components/Views/Analytics.jsx
@@ -2,10 +2,12 @@ import * as api from "../../api";
 
 export default function () {
   return (
-    <>
-      <div className="text-center pt-[35vh]">
-        <div className="font-extrabold text-4xl">🚧 Coming Soon 🚧</div>
+    <div className="bg-gradient-to-t from-[rgba(168,85,247,0.07)] flex items-center justify-center pb-[25vh] h-screen">
+      <div className="text-center">
+        <a className="font-extrabold text-4xl bg-gradient-to-r from-violet-600 to-indigo-600 inline-block text-transparent bg-clip-text py-10">
+          Coming Soon 🔧
+        </a>{" "}
       </div>
-    </>
+    </div>
   );
 }
