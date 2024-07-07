58e69f62c7955c5d8857b372d596a9f041712005
Ethan <gamesniper99@live.com>
2024-07-07 15:23:59 -0400

    Updated hourly handler to handle reponses

--- backend/src/MeetingReminder/hourly_handler.py
+++ backend/src/MeetingReminder/hourly_handler.py
@@ -14,35 +14,43 @@ def handler(event, context):
 
     now = int(datetime.now(pytz.timezone("US/Eastern")).timestamp()) * 1000
     meetings = table.scan(
-        FilterExpression=Attr("end_time").gt(now)
-        & Attr("end_time").lt(now + 60 * 60 * 1000),
+        FilterExpression=Attr("end_time").gt(
+            int(datetime.now(pytz.timezone("US/Eastern")).timestamp()) * 1000
+        ),
+        # FilterExpression=Attr("end_time").gt(now)
+        # & Attr("end_time").lt(now + 60 * 60 * 1000),
     )[
         "Items"
     ]  # between now and one hour from now
 
     for meeting in meetings:
-        if meeting["contact_method"] == "Email":
-            send_email(
-                meeting["contact_value"], meeting_reminder_email(meeting["start_time"])
-            )
-        elif meeting["contact_method"] == "Phone":
-            send_text(
-                meeting["contact_value"], meeting_reminder_text(meeting["start_time"])
-            )
-        elif meeting["contact_method"] == "Phone & Email":
-            send_email(
-                meeting["contact_value"], meeting_reminder_email(meeting["start_time"])
-            )
-            send_text(
-                meeting["contact_value"], meeting_reminder_text(meeting["start_time"])
-            )
+        email = meeting_reminder_email(
+            meeting["start_time"],
+            meeting["meeting_name"],
+            meeting["outbound_contact_value"],
+        )
+        text = meeting_reminder_text(
+            meeting["start_time"],
+            meeting["meeting_name"],
+            meeting["outbound_contact_value"],
+        )
 
+        if meeting["contact_method"] == "Email":
+            # send reminder via email
+            send_email(meeting["contact_value"], email)
+        elif meeting["contact_method"] == "Phone":
+            # send reminder via phone
+            send_text(meeting["contact_value"], text, meeting["id"])
+        elif meeting["contact_method"] == "Phone & Email":
+            # send reminder via email and phone
+            send_email(meeting["contact_value"], email)
+            send_text(meeting["contact_value"], text, meeting["id"])
     return {
         "reminders_sent": len(meetings),
     }
 
 
-def send_text(phone, message):
+def send_text(phone, message, meeting_id):
     pinpoint = boto3.client("pinpoint-sms-voice-v2")
 
     # get sender_id
@@ -55,11 +63,21 @@ def send_text(phone, message):
 
     originator_id = phone_nums["PhoneNumbers"][0]["PhoneNumberId"]
 
-    pinpoint.send_text_message(
+    response = pinpoint.send_text_message(
         DestinationPhoneNumber=f"+1{phone}",
         OriginationIdentity=originator_id,
         MessageBody=message,
         MessageType="TRANSACTIONAL",
+    )
+
+    # update latest message id
+    table = boto3.resource("dynamodb").Table(
+        os.environ["SCHEDULEDMEETINGSTABLE_TABLE_NAME"]
+    )
+    table.update_item(
+        Key={"id": meeting_id},
+        UpdateExpression="SET latest_message_id = :val",
+        ExpressionAttributeValues={":val": response["MessageId"]},
     )
 
 
@@ -84,19 +102,37 @@ def send_email(email, html):
     )
 
 
-def meeting_reminder_email(start_time):
-    t = datetime.fromtimestamp(float(start_time) / 1000)
+def meeting_reminder_email(time, meeting_name, outbound_contact):
+    if meeting_name == "":
+        meeting_name = "your meeting"
+
+    t = datetime.fromtimestamp(float(time) / 1000).strftime("%-I:%M %p")
+    s = f"Just a reminder that {meeting_name} starts in less than an hour! (@ {t})."
+
+    if outbound_contact != "":
+        s += f" You will be contacted by {outbound_contact}."
+
     return f"""
     <html>
     <body>
     <p>
-        Just a reminder than your meeting starts in less than an hour! ({t.date()} at {t.time()}).
+        {s}
     </p>
     </body>
     </html>
     """
 
 
-def meeting_reminder_text(start_time):
-    t = datetime.fromtimestamp(float(start_time) / 1000)
-    return f"Just a reminder than your meeting starts in less than an hour! ({t.date()} at {t.time()}) ⌛"
+def meeting_reminder_text(time, meeting_name, outbound_contact):
+    if meeting_name == "":
+        meeting_name = "your meeting"
+
+    t = datetime.fromtimestamp(float(time) / 1000).strftime("%-I:%M %p")
+    s = f"Just a reminder that {meeting_name} starts in less than an hour! (@ {t})."
+
+    if outbound_contact != "":
+        s += f" You will be contacted by {outbound_contact}."
+
+    s += " Text C to confirm or X to cancel."
+
+    return s
