8dd136b7299f1c244dfcf83aa9d19872841ec64d
Ethan <gamesniper99@live.com>
2024-07-27 16:19:54 -0400

    Added auto hide snackbar

--- frontend/src/Contexts/Snackbar.jsx
+++ frontend/src/Contexts/Snackbar.jsx
@@ -1,9 +1,17 @@
-import { useState, createContext } from "react";
+import { useState, createContext, useEffect } from "react";
 import Snackbar from "../Components/UI/Snackbar";
 export const SnackbarContext = createContext();
 
 export function SnackbarProvider({ children }) {
   const [snackbarMessage, setSnackbarMessage] = useState(null);
+
+  useEffect(() => {
+    if (snackbarMessage) {
+      setTimeout(() => {
+        setSnackbarMessage(null);
+      }, 3000);
+    }
+  }, [snackbarMessage]);
 
   return (
     <SnackbarContext.Provider
